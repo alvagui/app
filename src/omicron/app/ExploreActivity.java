42b7f4e63f460bced56ec96f18c57e330f8d0c79
AAGUILAR <aaguilar@jnovo.OMICRONSL.local>
2014-05-09 11:15:14 +0200

    Sync activity added

--- src/omicron/app/ExploreActivity.java
+++ src/omicron/app/ExploreActivity.java
@@ -1,21 +1,23 @@
 package omicron.app;
 
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class ExploreActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_explore);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -27,8 +29,8 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.explore, menu);
+		return true;
 	}
 
 	@Override
@@ -39,9 +41,6 @@ public class MainActivity extends ActionBarActivity {
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			startActivity(new Intent(this, SettingsActivity.class));
-			return true;
-		} else if (id == R.id.action_sync){
-			startActivity(new Intent(this, SyncActivity.class));
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -58,26 +57,10 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			
-			
+			View rootView = inflater.inflate(R.layout.fragment_explore,
+					container, false);
 			return rootView;
 		}
 	}
-	
-	// Button implemented
-	public void openTabbedActivity(View view)
-	{
-		Intent intent = new Intent(this, TabbedActivity.class);
-		startActivity(intent);
-	}
 
-	// Button implemented
-	public void openExploreActivity(View view)
-	{
-		Intent intent = new Intent(this, ExploreActivity.class);
-		startActivity(intent);
-	}
-	
 }
